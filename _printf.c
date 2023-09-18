abfbfa4d10b521654cf43193e34c1e07bfe9da44
Mennarefat <mennarefaat22@gmail.com>
2023-09-18 13:39:59 -0700

    task1

--- _printf.c
+++ _printf.c
@@ -1,10 +1,6 @@
 #include "main.h"
-void pbuff(char buffer[], int *bind);
-int get_sz(const char *format, int *i);
-int get_flgs(const char *format, int *i);
-int get_precisn(const char *format, int *i, va_list args);
-int get_wdth(const char *format, int *i, va_list args);
-int handle_s_count(const char *fmt, int *ind, va_list args, char buffer[],int flgs, int wdth, int precisn, int sz);
+int inchr(char c);
+int insert(char *c);
 /**
 *_printf - Printf to stdout formatted text
 *@format: format specifier
@@ -12,38 +8,35 @@ int handle_s_count(const char *fmt, int *ind, va_list args, char buffer[],int fl
 */
 int _printf(const char *format, ...)
 {
-int i, s_count, count = 0;
-int flgs, wdth, precisn, sz, bind = 0;
-char buffer[BUFF_SIZE];
+unsigned int i, s_count, count = 0;
 va_list args;
 if (!format || (format[0] == '%' && format[1] == '\0'))
 return (-1);
 va_start(args, format);
-for (i = 0; format && format[i] != '\0'; i++)
+for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] != '%')
 {
-buffer[bind++] = format[i];
-if (bind == BUFF_SIZE)
-pbuff(buffer, &bind);
-count++;
+inchr(format[i]);
 }
-else
+else if (format[i] == '%' && format[i + 1] == '0')
 {
-pbuff(buffer, &bind);
-flgs = get_flgs(format, &i);
-wdth = get_wdth(format, &i, args);
-precisn = get_precisn(format, &i, args);
-sz = get_sz(format, &i);
-++i;
-s_count = handle_s_count(format, &i, args, buffer, flgs, wdth, precisn, sz);
-if (s_count == -1)
-return (-1);
-count += s_count;
+inchr(va_arg(args, int));
+i++;
+}
+else if (format[i + 1] == 's')
+{
+s_count = insert(va_arg(args, char *));
+i++;
+count += (s_count - 1);
+}
+else if (format[i + 1] == '%')
+{
+inchr('%');
 }
 }
-pbuff(buffer, &bind);
-va_end(args);
+count++;
 return (count);
+va_end(args);
 }
 
--- main.h
+++ main.h
@@ -3,23 +3,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#define BUFF_SIZE 1024
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-#define S_LONG 2
-#define S_SHORT 1
-void pbuff(char buffer[], int *bind);
-int print_char(va_list types, char buffer[],int flgs, int wdth, int precisn, int sz);
-int print_string(va_list types, char buffer[],int flgs, int wdth, int precisn, int sz);
-int print_percent(va_list types, char buffer[],int flgs, int wdth, int precisn, int sz);
-int print_int(va_list types, char buffer[],int flgs, int wdth, int precisn, int sz);
 int _printf(const char *format, ...);
-int get_sz(const char *format, int *i);
-int get_flgs(const char *format, int *i);
-int get_precisn(const char *format, int *i, va_list args);
-int get_wdth(const char *format, int *i, va_list args);
-int handle_s_count(const char *fmt, int *ind, va_list args, char buffer[],int flgs, int wdth, int precisn, int sz);
+int inchr(char c);
+int insert(char *c);
 #endif
