fe645b1415a47a88d2939ed4346bf58fcc3f0029
khouloud <rajie.khouloud@gmail.com>
2023-09-19 14:50:46 -0700

    functions

--- main.h
+++ main.h
@@ -7,5 +7,13 @@
 int _printf(const char *format, ...);
 int inchr(char c);
 int insert(char *c);
-int putss (char *c);
+
+
+int print_char(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],int flags, int width, int precision, int size);
+
+
 #endif
