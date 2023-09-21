0e0ff027707aa41f9f611c12f2f2661a0e5a4101
khouloud <rajie.khouloud@gmail.com>
2023-09-21 10:37:26 -0700

    printf.c

--- _printf.c
+++ _printf.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "main.h"
-int inchr(char c);
-int insert(char *c);
+
 /**
 *_printf - Printf to stdout formatted text
 *@format: format specifier
@@ -8,35 +10,44 @@ int insert(char *c);
 */
 int _printf(const char *format, ...)
 {
-unsigned int i, s_count, count = 0;
-va_list args;
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-va_start(args, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-inchr(format[i]);
-}
-else if (format[i] == '%' && format[i + 1] == '0')
-{
-inchr(va_arg(args, int));
-i++;
-}
-else if (format[i + 1] == 's')
-{
-s_count = insert(va_arg(args, char *));
-i++;
-count += (s_count - 1);
-}
-else if (format[i + 1] == '%')
-{
-inchr('%');
-}
-}
-count++;
+    unsigned int i ,count=0 ,s_count;
+
+    va_list args;
+
+    va_start(args, format);
+
+    for( i=0 ; format[i] != '\0' ; i++)
+    {
+        if ( format[i] != '%')
+        {
+            putchar(format[i]);
+        }
+        else {
+                if (format[i+1] == 'c')
+        {
+            putchar(va_arg(args,int));
+            i++;
+        }
+          else{
+                if (format[i+1] == 's')
+                {
+                   s_count = puts(va_arg(args, char *));
+                   i++;
+                   count += (s_count - 1);
+                }
+                else{
+                    if (format[i+1] == '%')
+                        {
+                        putchar('%');
+                        }
+                }
+
+        }
+        }
+        count += 1;
+    }
+
+
 return (count);
-va_end(args);
 }
 
